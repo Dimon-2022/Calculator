18607494310e6a05bb835b9f02c393bb3363941b
dima.moisieienko <dimonmail09@gmail.com>
2025-04-25 18:41:25 +0300

    finished calculator app

--- src/App.jsx
+++ src/App.jsx
@@ -26,71 +26,75 @@ import "./index.css";
 // Use this method for all operations in the calculator.
 
 function Calculator() {
-  const [input, setInput] = useState(null);
+  const [input, setInput] = useState("0");
 
-  function incrementNumber() {
-    setInput((prev) => Number(prev) + 1);
+  function addOne() {
+    const currentInput = eval(input);
+    setInput(currentInput + 1).toString();
   }
 
-  function decrementNumber() {
-    setInput((prev) => Number(prev) - 1);
+  function subtractOne() {
+    const currentInput = eval(input);
+    setInput(currentInput - 1).toString();
   }
 
-  function calculate(e) {
-    const btn = e.target.textContent; //=
+  function handleClick(value) {
+    const symbols = ["+", "-", "/", "*", "=", "."];
 
-    if (btn === "=") {
-      setInput((prev) => eval(prev));
+    //const isDoubleSymbol = symbols.find(symbol=>symbol === input.slice(-1));
+    console.log(input.slice(-1));
 
-      console.log("Dima");
-    } else if (btn === "C") {
+    if (value === "C") {
       setInput("0");
-    } else {
-      setInput((prev) => (prev === 0 ? btn : String(prev) + btn));
+      return;
+    } else if (value === "=") {
+      setInput((prev) => eval(prev).toString());
+      return;
     }
+    setInput((prev) => (prev === "0" ? value : prev + value));
   }
 
   return (
     <div className="calculator-container">
       <h1 className="calculator-title">UseState Calculator</h1>
       <div className="calculator">
-        <div className="display">{input || 0}</div>
+        <div className="display">{input}</div>
         <div className="increment-buttons">
-          <button className="increment" onClick={incrementNumber}>
+          <button className="increment" onClick={addOne}>
             +1
           </button>
-          <button className="decrement" onClick={decrementNumber}>
+          <button className="decrement" onClick={subtractOne}>
             -1
           </button>
         </div>
         <div className="buttons">
-          <button onClick={calculate}>1</button>
-          <button onClick={calculate}>2</button>
-          <button onClick={calculate}>3</button>
-          <button className="operator" onClick={calculate}>
+          <button onClick={() => handleClick("1")}>1</button>
+          <button onClick={() => handleClick("2")}>2</button>
+          <button onClick={() => handleClick("3")}>3</button>
+          <button className="operator" onClick={() => handleClick("+")}>
             +
           </button>
-          <button onClick={calculate}>4</button>
-          <button onClick={calculate}>5</button>
-          <button onClick={calculate}>6</button>
-          <button className="operator" onClick={calculate}>
+          <button onClick={() => handleClick("4")}>4</button>
+          <button onClick={() => handleClick("5")}>5</button>
+          <button onClick={() => handleClick("6")}>6</button>
+          <button className="operator" onClick={() => handleClick("-")}>
             -
           </button>
-          <button onClick={calculate}>7</button>
-          <button onClick={calculate}>8</button>
-          <button onClick={calculate}>9</button>
-          <button className="operator" onClick={calculate}>
+          <button onClick={() => handleClick("7")}>7</button>
+          <button onClick={() => handleClick("8")}>8</button>
+          <button onClick={() => handleClick("9")}>9</button>
+          <button className="operator" onClick={() => handleClick("*")}>
             ×
           </button>
-          <button onClick={calculate}>0</button>
-          <button onClick={calculate}>,</button>
-          <button className="equals" onClick={calculate}>
+          <button onClick={() => handleClick("0")}>0</button>
+          <button onClick={() => handleClick(".")}>,</button>
+          <button className="equals" onClick={() => handleClick("=")}>
             =
           </button>
-          <button className="operator" onClick={calculate}>
+          <button className="operator" onClick={() => handleClick("/")}>
             ÷
           </button>
-          <button className="clear" onClick={calculate}>
+          <button className="clear" onClick={() => handleClick("C")}>
             C
           </button>
         </div>
